c96ef68f862356036d43b256117d225c0695ca0a
Krzysztof Furtak <krzysztof.furtak@gmail.com>
2021-04-13 14:23:58 +0200

    fix: build problem

--- lib/components/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as Paginate } from './paginate';
-//# sourceMappingURL=index.d.ts.map
--- lib/helpers/PagerCalculations.d.ts
+++
@@ -1,8 +0,0 @@
-declare class PagerCalculations {
-    static pagesCount(elementsCount: number, pageSize: number): number;
-    static calcStartRange(currentPage: number, pageSize: number): number;
-    static calcEndRange(currentPage: number, pageSize: number): number;
-    static canDisplayElement(index: number, currentPage: number, pageSize: number): boolean;
-}
-export default PagerCalculations;
-//# sourceMappingURL=PagerCalculations.d.ts.map
